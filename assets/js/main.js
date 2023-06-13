3a50a01a464ed8888a2d68450e8331ecb8157ee6
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-06-13 14:12:13 -0300

    Feat: Iniciando projeto (header)

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+// Open / Close menu navbar
+const openMenuBtn = document.getElementById('open')
+const closeMenuBtn = document.getElementById('close')
+const menuNavbar = document.querySelector('.navbar__menu')
+const overlay = document.querySelector('.overlay')
+
+const openMenu = () => {
+    menuNavbar.style.right = '.1rem'
+    openMenuBtn.style.display = 'none'
+    closeMenuBtn.style.display = 'block'
+}
+
+openMenuBtn.addEventListener('click', openMenu)
+
+const closeMenu = () => {
+    menuNavbar.style.right = '-100%'
+    closeMenuBtn.style.display = 'none'
+    openMenuBtn.style.display = 'block'
+}
+
+closeMenuBtn.addEventListener('click', closeMenu)
+overlay.addEventListener('click', closeMenu)
+
+
+// Close menu navbar when clicking any option
+if(window.innerWidth <= 992) {
+    document.querySelectorAll('.navbar__menu .menu__item .menu__link').forEach(navItem => {
+        navItem.addEventListener('click', () => {
+            closeMenu()
+        })
+    })
+}
+
+
+// Change menu navbar color when scrolling
+window.addEventListener('scroll', () => {
+    document.querySelector('.header').classList.toggle('scrolling', window.scrollY)
+})
