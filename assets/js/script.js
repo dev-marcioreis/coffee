8fb08d33b409449f16d3068dbe5aed66aa49a8b7
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-06-13 16:10:53 -0300

    Fix: GSAP menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,2 @@
-gsap.from('.menu__item', {opacity: 0, duration: 1, delay: 0, y: -60})
 gsap.from('.home__content', {opacity: 0, duration: 1, delay: 1, x: 30})
 gsap.from('.home__image', {opacity: 0, duration: 1, delay: 1, x: -30})
